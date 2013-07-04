a105e68157577c24cd418c9eec3bda3000a39085
utsavkesharwani <utsav.kesharwani@gmail.com>
2013-07-04 15:57:55 +0530

    updated constants

--- 04-07-13/a3.rb
+++ 04-07-13/a3.rb
@@ -1,11 +1,11 @@
 constants = {
-	'1' => 'I',
-	'5' => 'V',
-	'10' => 'X',
-	'50' => 'L',
-	'100' => 'C',
-	'500' => 'D',
-	'1000' => 'M'
+	1 => 'I',
+	5 => 'V',
+	10 => 'X',
+	50 => 'L',
+	100 => 'C',
+	500 => 'D',
+	1000 => 'M'
 }
 
 puts "Enter a number"
@@ -17,14 +17,14 @@ index = 10**(n.length - 1)
 n.split('').each do |s|
 	digit = s.to_i
 	if [1, 2, 3].include? digit
-		digit.times { |x| roman_num += constants[(index).to_s] }
+		digit.times { |x| roman_num += constants[index] }
 	elsif [6, 7, 8].include? digit
-		roman_num += constants[(5 * index).to_s]
-		(digit - 5).times { |x| roman_num += constants[(index).to_s] }
+		roman_num += constants[(5 * index)]
+		(digit - 5).times { |x| roman_num += constants[index] }
 	elsif [4, 9].include? digit
-		roman_num += constants[index.to_s] + constants[((digit + 1) * index).to_s]
+		roman_num += constants[index] + constants[((digit + 1) * index)]
 	elsif digit == 5
-		roman_num += constants[(5 * index).to_s]
+		roman_num += constants[(5 * index)]
 	end
 	index /= 10
 end
